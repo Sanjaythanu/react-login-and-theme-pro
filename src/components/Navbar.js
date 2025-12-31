9cca533d12b2de872978f165d1af12604fd0d2f3
Sanjay Sri Thanu Malaiyaan <sanjaychittu2005@gmail.com>
2025-12-31 20:29:03 +0530

    hil

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,6 @@ function Navbar() {
     <nav
       style={{
         padding: "15px",
-        display: "flex",
         justifyContent: "space-between",
         background: theme === "light" ? "#eee" : "#333",
         color: theme === "light" ? "#000" : "#fff",
